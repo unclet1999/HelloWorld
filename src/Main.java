35888082301ae0018fc01b02c7cb4afd05516c3d
uncle <real.friend2013@outlook.com>
2019-05-23 04:54:50 +0430

    Delete

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Adding Code");
-        System.out.println("What the Heck?");
     }
 }
