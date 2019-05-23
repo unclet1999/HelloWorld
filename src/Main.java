24241ed21a1c017bafb829b7802b0ad2689da250
uncle <real.friend2013@outlook.com>
2019-05-23 05:08:34 +0430

    Furious

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Adding Code");
-        System.out.println("What the Heck?");
+        System.out.println("I changed some stuff");
     }
 }
